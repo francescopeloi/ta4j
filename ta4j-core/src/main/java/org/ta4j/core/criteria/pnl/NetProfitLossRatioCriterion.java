03d1c4c20cdb4e7741bb77f20803125bce98b785
Francesco <francesco.peloi@gmail.com>
2025-06-11 11:50:20 +0300

    Split ProfitLossRatioCriterion into net and gross versions

---
+++ ta4j-core/src/main/java/org/ta4j/core/criteria/pnl/NetProfitLossRatioCriterion.java
@@ -0,0 +1,9 @@
+package org.ta4j.core.criteria.pnl;
+
+/** Net profit/loss ratio criterion. */
+public class NetProfitLossRatioCriterion extends AbstractProfitLossRatioCriterion {
+
+    public NetProfitLossRatioCriterion() {
+        super(new NetAverageProfitCriterion(), new NetAverageLossCriterion());
+    }
+}
